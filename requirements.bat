86ab38db98b179e44614d4a9974ce7bd58aed762
James <p1n3tr335@gmail.com>
2018-07-07 16:32:49 -0400

    Macaroni

--- requirements.bat
+++ requirements.bat
@@ -1,5 +1,6 @@
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://chromedriver.storage.googleapis.com/2.40/chromedriver_win32.zip', 'chromedriver_win32.zip')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://stahlworks.com/dev/unzip.exe', 'unzip.exe')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://pastebin.com/raw/amqJduwg', 'ez_dlc.py')"
 unzip chromedriver_win32.zip
 del chromedriver_win32.zip
 del unzip.exe
