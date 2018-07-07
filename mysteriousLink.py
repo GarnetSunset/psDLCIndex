771a0f8eb900c3f51a4e51a6c7efe88c15ecc269
James <p1n3tr335@gmail.com>
2018-07-07 16:46:07 -0400

    hmm

---
+++ mysteriousLink.py
@@ -0,0 +1,20 @@
+import os
+
+try: input = raw_input
+except NameError: pass
+
+dragNDrop = ""
+
+if dragNDrop == '':
+    fileName = input("Input the file with extension")
+else:
+    fileOnly = dragNDrop.rfind('\\') + 1
+    fileName = dragNDrop[fileOnly:]
+    
+with open(fileName) as f:
+    for line in f:
+       brexit = line.index('|')
+       titleID = line[:brexit]
+       name = line[brexit+1:]
+       os.system("ez_dlc.py " + titleID.strip() + " " + "\"" + name.strip() + "\"")
+       
