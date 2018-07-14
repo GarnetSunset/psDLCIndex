af45d2f8e8bc66c488e4cfb0f910b95d1cec5241
James <p1n3tr335@gmail.com>
2018-07-14 09:57:51 -0400

    Fixes, thanks to reddit

--- requirements.bat
+++ requirements.bat
@@ -1,5 +1,2 @@
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://pastebin.com/raw/amqJduwg', 'ez_dlc.py')"
-unzip chromedriver_win32.zip
-del chromedriver_win32.zip
-del unzip.exe
 C:\Python27\Scripts\pip.exe install -q -r requirements.txt
